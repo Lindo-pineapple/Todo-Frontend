36f67bf83d8d71696e8643f5ae2baad71002e68f
Lindokuhle <lindokuhle.dlomo@pineapple.co.za>
2024-07-10 14:09:12 +0200

    FE fixes

--- App.tsx
+++ App.tsx
@@ -1,118 +1,67 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  main: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    alignContent: 'flex-start',
+    justifyContent: 'flex-start',
+    backgroundColor: '#fafaff',
+    width: '100%',
+    height: '100%',
   },
-  sectionTitle: {
+  title: {
+    fontSize: 64,
+    fontWeight: 'bold',
+    color: 'black',
+    marginHorizontal: 10,
+    marginVertical: 20,
+    backgroundColor: 'green',
+  },
+  topIcons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+  },
+  menuIcon: {
+    fontWeight: 'bold',
     fontSize: 24,
-    fontWeight: '600',
+    color: 'black',
+    backgroundColor: 'red',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  bellIcon: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: 'black',
+    backgroundColor: 'red',
   },
-  highlight: {
-    fontWeight: '700',
+  bottomContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  plus: {
+    fontWeight: 'bold',
+    fontSize: 34,
+    color: 'white',
+    backgroundColor: 'blue',
+    alignSelf: 'flex-end',
   },
 });
 
-export default App;
+export default function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.main}>
+      <SafeAreaView style={styles.topIcons}>
+        <Text style={styles.menuIcon}>Icon</Text>
+        <Text style={styles.bellIcon}>Icon</Text>
+      </SafeAreaView>
+      <Text style={styles.title}>Today</Text>
+      <SafeAreaView style={styles.bottomContainer}>
+        <Text style={styles.plus}>Plus</Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
+}
