fae041b223890511534e81ef644fc8680e48dd43
Lindokuhle <lindokuhle.dlomo@pineapple.co.za>
2024-07-12 10:14:43 +0200

    Added colors

--- App.tsx
+++ App.tsx
@@ -1,28 +1,37 @@
 import React, {useEffect, useState} from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, Text} from 'react-native';
 import {addTodo, getTodos} from './src/api/api';
 import TodoItem from './src/components/TodoItem';
 import TodoComponent from './src/components/TodoComponent';
 import AddTodo from './src/components/AddTodo';
 
+//BLUE COLOR = #0076FE
+//FADE BACK COLOR WHEN MODAL IS UP = #BEC6D1
+
 const styles = StyleSheet.create({
   main: {
     display: 'flex',
+    flexWrap: 'wrap',
     flex: 1,
     flexDirection: 'column',
     alignContent: 'flex-start',
     justifyContent: 'flex-start',
-    backgroundColor: '#fafaff',
+    backgroundColor: '#EFF2F7',
     width: '100%',
     height: '100%',
+  },
+  contentContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    alignContent: 'space-around',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 64,
     fontWeight: 'bold',
-    color: 'black',
+    color: '#2D2E30',
     marginHorizontal: 10,
-    marginVertical: 20,
-    backgroundColor: 'green',
   },
   topIcons: {
     display: 'flex',
@@ -43,31 +52,29 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
   bottomContainer: {
-    flex: 1,
     justifyContent: 'flex-end',
-    backgroundColor: 'orange',
   },
   plus: {
     fontWeight: 'bold',
     fontSize: 34,
     color: 'white',
-    backgroundColor: 'blue',
+    backgroundColor: '#0076FE',
     alignSelf: 'flex-end',
   },
 });
 
 export default function App(): React.JSX.Element {
-  const [todos, setTodos] = useState<ITodo[]>([]);
+  // const [todos, setTodos] = useState<ITodo[]>([]);
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
+  // useEffect(() => {
+  //   fetchTodos();
+  // }, []);
 
-  const fetchTodos = async (): Promise<void> => {
-    await getTodos()
-      .then((todos: ITodo[] | any) => setTodos(todos))
-      .catch((err: Error) => console.log(err));
-  };
+  // const fetchTodos = async (): Promise<void> => {
+  //   await getTodos()
+  //     .then((todos: ITodo[] | any) => setTodos(todos))
+  //     .catch((err: Error) => console.log(err));
+  // };
 
   return (
     <SafeAreaView style={styles.main}>
@@ -76,23 +83,10 @@ export default function App(): React.JSX.Element {
         <Text style={styles.menuIcon}>Icon</Text>
         <Text style={styles.bellIcon}>Icon</Text>
       </SafeAreaView>
-      <Text style={styles.title}>Today</Text>
-      {/* {todos.map((todo: ITodo) => (
-        <TodoItem
-          key={todo._id}
-          updateTodo={() => {}}
-          deleteTodo={() => {}}
-          todo={todo}
-        />
-      ))} */}
-      {todos.map((todo: ITodo) => (
-        <TodoComponent
-          key={todo._id}
-          updateTodo={() => {}}
-          deleteTodo={() => {}}
-          todo={todo}
-        />
-      ))}
+      <SafeAreaView style={styles.contentContainer}>
+        <Text style={styles.title}>Today</Text>
+        <TodoItem />
+      </SafeAreaView>
       <SafeAreaView style={styles.bottomContainer}>
         <Text style={styles.plus}>Plus</Text>
       </SafeAreaView>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,39 +1,158 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  FlatList,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 
-type Props = TodoProps & {
-  updateTodo: (todo: ITodo) => void;
-  deleteTodo: (_id: string) => void;
+// DUMMY DATA FOR DESIGN PURPOSES
+const TODOS: ITodo[] | any = [
+  {
+    _id: '668d4cf8dd3d76c20f8606be',
+    todo: 'Create Todos App',
+    description: 'Create a Fullstack React Native Todo App.',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668e2ed37092951c758c3016',
+    todo: 'Todo to be Deleted For delete purposes',
+    description:
+      'Make sure every single part of the backend works as it should.',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668fe5d286ac87026d767cc4',
+    todo: 'Work on Front-End',
+    description:
+      'Do the Frontend Design, and make it it look like it is supposed to.',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668fe5ed86ac87026d767cc6',
+    todo: 'Fix Styling and fonts',
+    description: 'Fix STyling and fonts',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668fe63a86ac87026d767cc8',
+    todo: 'Implement Auth',
+    description: 'Allow Users to Register and Login and use the Application.',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668fe66786ac87026d767ccb',
+    todo: 'Creat Login And Register Page',
+    description: 'Create the Login and registration Pages',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668fe68b86ac87026d767ccd',
+    todo: 'Create a landing Page',
+    description: 'Create a Landin Page for the application',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+  {
+    _id: '668fe6a686ac87026d767ccf',
+    todo: 'Add Loaders',
+    description: 'While the application is performing Async Tasks Add Loaders.',
+    isDone: false,
+    userId: '668d40069e8cecf19b90a19f',
+    __v: 0,
+  },
+];
+
+type ItemProps = {
+  todo: ITodo;
+  onPress: () => void;
 };
 
-const Todo: React.FC<Props> = ({todo, updateTodo, deleteTodo}) => {
-  const checkTodo: string = todo.isDone ? 'line-through' : '';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    marginVertical: 8,
+    marginHorizontal: 8,
+    borderRadius: 15,
+    backgroundColor: '#FFFFFF',
+  },
+  contentContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 20,
+  },
+  checkbox: {
+    alignSelf: 'flex-start',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    width: '10%',
+    backgroundColor: 'white',
+    color: 'white',
+  },
+  itemDetails: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  title: {
+    alignSelf: 'flex-start',
+    fontSize: 16,
+    backgroundColor: 'white',
+  },
+  dateText: {
+    alignSelf: 'flex-start',
+    fontSize: 12,
+    backgroundColor: 'white',
+  },
+});
+
+const Todo = ({todo, onPress}: ItemProps) => (
+  <TouchableOpacity onPress={onPress} style={styles.item}>
+    <SafeAreaView style={styles.contentContainer}>
+      <TouchableOpacity style={styles.checkbox}>
+        <Text>{todo.isDone.toString()}</Text>
+      </TouchableOpacity>
+      <SafeAreaView style={styles.itemDetails}>
+        <Text style={styles.title}>{todo.todo}</Text>
+        <Text style={styles.dateText}>{todo.userId}</Text>
+      </SafeAreaView>
+    </SafeAreaView>
+  </TouchableOpacity>
+);
+
+const TodoItem: React.FC<Props> = () => {
+  const renderItem = ({item}: {item: ITodo}) => {
+    return <Todo todo={item} onPress={() => {}} />;
+  };
+
   return (
-    // <div className="Card">
-    //   <div className="Card--text">
-    //     <h1 className={checkTodo}>{todo.name}</h1>
-    //     <span className={checkTodo}>{todo.description}</span>
-    //   </div>
-    //   <div className="Card--button">
-    //     <button
-    //       onClick={() => updateTodo(todo)}
-    //       className={todo.isDone ? `hide-button` : 'Card--button__done'}>
-    //       Complete
-    //     </button>
-    //     <button
-    //       onClick={() => deleteTodo(todo._id)}
-    //       className="Card--button__delete">
-    //       Delete
-    //     </button>
-    //   </div>
-    // </div>
-    <SafeAreaView>
-      <Text>{todo.todo}</Text>
-      <Text>{todo.description}</Text>
-      <Text>Is Done: {todo.isDone.toString()}</Text>
-      <Text>By user: {todo.userId}</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={TODOS}
+        renderItem={renderItem}
+        keyExtractor={item => item._id}
+      />
     </SafeAreaView>
   );
 };
 
-export default Todo;
+export default TodoItem;
