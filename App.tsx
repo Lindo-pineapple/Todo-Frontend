90cf8ff789d39880cc1292e3c5acdb7199e29b1a
Lindokuhle <lindokuhle.dlomo@pineapple.co.za>
2024-07-18 10:26:56 +0200

    fixups on splash screen and app icon

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import DrawerNav from './src/screens/Navigation/DrawerNavigation';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App(): React.JSX.Element {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 500);
+  });
   return (
     <NavigationContainer>
       <DrawerNav />
